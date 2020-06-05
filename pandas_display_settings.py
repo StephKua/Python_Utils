2ef457e42636cfa03f6f6fc5a92b3fb6d4f63bc7
StephKua <stephenkkz97@gmail.com>
2020-06-05 13:52:55 +0800

    added random and pandas utils

---
+++ pandas_display_settings.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+
+pd.set_option('display.max_rows', 999)
+pd.set_option('display.max_columns', 999)
+pd.set_option('display.width', 999)
+
+np.set_printoptions(threshold=sys.maxsize)
---
+++ random_seed.py
@@ -0,0 +1,9 @@
+def seed_torch(seed):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    
+tf.random.set_seed(SEED)
